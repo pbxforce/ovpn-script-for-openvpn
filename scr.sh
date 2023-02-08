5c64067be611bb1807cb8f7125785f97f837ba90
pbxforce <120632049+pbxforce@users.noreply.github.com>
2023-02-08 12:52:41 +0530

    Add files via upload

---
+++ scr.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat client-master.ovpn > $1.ovpn
+file=$1.ovpn
+echo "<ca>" >> $file;
+cat $2 >> $file;
+echo "</ca>" >> $file;
+echo "<cert>" >> $file;
+cat $3 >> $file;
+echo "</cert>" >> $file;
+echo "<key>" >> $file;
+cat $4 >> $file;
+echo "</key>" >> $file;
+
